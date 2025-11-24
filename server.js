fdeb8e0e57ba574ac95f532ac099a3e4119317be
yurii-leginchuk <yurij.leginchuk.dev@gmail.com>
2025-11-24 23:09:09 +0200

    fixes

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
